9c0865788cab2c4e9ca58d43a1f06e0db0847fb1
saurabh_sheth <saurabh_sheth@apple.com>
2018-12-22 20:17:29 -0800

    Updates

---
+++ precache-manifest.09c0ee7de924aa21edb5984f1312539a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea05e55c3ba8922d52f5",
+    "url": "/contactmanagera/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "ea05e55c3ba8922d52f5",
+    "url": "/contactmanagera/static/js/main.ea05e55c.chunk.js"
+  },
+  {
+    "revision": "00adf9f99622f5ac33fa",
+    "url": "/contactmanagera/static/css/1.cec7e7a3.chunk.css"
+  },
+  {
+    "revision": "00adf9f99622f5ac33fa",
+    "url": "/contactmanagera/static/js/1.00adf9f9.chunk.js"
+  },
+  {
+    "revision": "4a5e64e517aa50772bfb",
+    "url": "/contactmanagera/static/js/runtime~main.4a5e64e5.js"
+  },
+  {
+    "revision": "ff7f34711eb764dd9427f889077f9b78",
+    "url": "/contactmanagera/index.html"
+  }
+];
